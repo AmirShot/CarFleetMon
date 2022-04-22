698d7b92ed2acbbd181724c5103401a40108bed2
Amir <amir2004shot@gmail.com>
2022-04-22 17:57:34 +0300

    step 7 - new and closed connections hooks. need to add SQL

--- DataManager.py
+++ DataManager.py
@@ -5,30 +5,44 @@ from proto1_pb2 import *
 
 class DataManager:
     def __init__(self, connection_manager):
+        self._active_connections = dict()
         self._updatePeriod = 10
         self._connectionManager = connection_manager
         self._running = True
         self._thread = threading.Thread(target=self.update)
         self._thread.start()
 
+    def new_connection(self, id: str):
+        self._active_connections[id] = True
+
+    def init_connections(self):
+        for cw in self._active_connections:
+            self._active_connections[cw] = False
+
+    def close_connection(self, id: str):
+        self._active_connections.pop(id)
+
+
     def update(self):
         lock = threading.Lock()
         while self._running:
             lock.acquire()
+            self.init_connections()
             for cw in self._connectionManager.conWrap:
-
-                print(f"cw: {cw}")
                 car_data = CurrData()
-                #print(f"type: {self._connectionManager.conWrap[cw].update(5)}")
-
+                if not cw in self._active_connections:
+                    self.new_connection(cw)
                 if self._connectionManager.conWrap[cw].is_active():
                     data = self._connectionManager.conWrap[cw].update(5)
-                    print(f"data: {data}")
                     car_data.ParseFromString(data)
-                    print("Trying to print CarData")
                     print(car_data)
+            for cw in self._active_connections:
+                if not self._active_connections[cw]:
+                    self.close_connection(cw)
+
             lock.release()
-            time.sleep(11)
+            time.sleep(1)
+
 
 
 
