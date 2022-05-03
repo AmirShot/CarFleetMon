b103a3f0c58e82e8e870da735ac2d98a6bf778b3
Amir <amir2004shot@gmail.com>
2022-05-04 00:32:20 +0300

    step 12 tabl working - start of live data a little progress save before start of doing isActive and updating

--- CarFleetMon.py
+++ CarFleetMon.py
@@ -1,10 +1,12 @@
 from DataManager import DataManager
 from ConnectionManager import ConnectionManager
+import GuiTrial
 
 class CarFleetMon:
     def __init__(self):
         self._connection_manager = ConnectionManager("127.0.0.1")
         self._data_manager = DataManager(self._connection_manager)
+        self.GUI = GuiTrial.GUI(self._data_manager)
 
 if __name__ == "__main__":
     CFM = CarFleetMon()
--- GuiTrial.py
+++ GuiTrial.py
@@ -5,18 +5,21 @@ from PandasModel import PandasModel"""
 
 from PyQt5 import QtWidgets, QtCore, QtGui
 from PyQt5.QtWidgets import QApplication, QPushButton, QHBoxLayout, QTabWidget, QVBoxLayout, QWidget, QMainWindow, QGroupBox, QTableWidget,QTableWidgetItem
-from PyQt5.QtGui import QPainter
+from PyQt5.QtGui import QPainter, QBrush, QPen, QFont
+from PyQt5.QtCore import Qt
 from PyQt5.QtCore import pyqtSlot
 import sqlite3
+from DataManager import DataManager
 
 class ProgramWindow(QMainWindow):
-    def __init__(self):
+    def __init__(self, datamanager: DataManager):
         QMainWindow.__init__(self)
         self.homeDir = "."
         self.setup_main_window()
         self.set_window_layout()
         self.rowCounterTableStats = 0
-
+        self.clicked = False
+        self.dataManager = datamanager
 
 
     def setup_main_window(self):
@@ -153,32 +156,124 @@ class ProgramWindow(QMainWindow):
         return list(ret)
 
     def showLiveData(self, licencePlate):
-        d1 = QtWidgets.QDialog()
-        b1 = QtWidgets.QPushButton("Hello World", d1)
-        b1.move(500,250)
-        d1.setGeometry(100, 100, 750, 500)
-        d1.setWindowTitle("Dialog!")
-        lbl1 = QtWidgets.QLabel("Nothing", d1)
-        current_row = self.table1.currentRow()
-        lbl1.setText(self.table1.item(current_row, 0).text())
-        painter = QPainter()
-        painter.begin(d1)
-        painter.setRenderHint(QPainter.Antialiasing)
-        painter.setPen(QtCore.Qt.black)
-        painter.setBrush(QtCore.Qt.white)
-        painter.drawLine(0, 0, 200, 200)
+        d1 = dialog(self.table1, licencePlate)
+
+class dialog:
+    def __init__(self,table1:QTableWidget, LicencePlate):
+        self.d1 = QtWidgets.QDialog()
+        self.b1 = QtWidgets.QPushButton("Hello World", self.d1)
+        self.b1.move(500, 250)
+        self.d1.setGeometry(100, 100, 750, 500)
+        self.d1.setWindowTitle("Dialog!")
+        self.lbl1 = QtWidgets.QLabel("Nothing", self.d1)
+        self.current_row = table1.currentRow()
+        self.lbl1.setText(table1.item(self.current_row, 0).text())
+
+        dif = 50
+        start = 80
+        xPos = 70
+        start2 = start - 20
+        xPos2 = 600
+
+        self.lblLine = QtWidgets.QLabel("-------------------------------------------------------------", self.d1)
+        self.lblLine.setFont(QFont("Ariel", 20))
+        self.lblLine.move(xPos, start)
+
+        self.lblLine1 = QtWidgets.QLabel("-------------------------------------------------------------", self.d1)
+        self.lblLine1.setFont(QFont("Ariel", 20))
+        self.lblLine1.move(xPos, start + dif * 1)
+
+        self.lblLine2 = QtWidgets.QLabel("-------------------------------------------------------------", self.d1)
+        self.lblLine2.setFont(QFont("Ariel", 20))
+        self.lblLine2.move(xPos, start + dif * 2)
+
+        self.lblLine3 = QtWidgets.QLabel("-------------------------------------------------------------", self.d1)
+        self.lblLine3.setFont(QFont("Ariel", 20))
+        self.lblLine3.move(xPos, start + dif * 3)
+
+        self.lblLine4 = QtWidgets.QLabel("-------------------------------------------------------------", self.d1)
+        self.lblLine4.setFont(QFont("Ariel", 20))
+        self.lblLine4.move(xPos, start + dif * 4)
+
+        self.lblLine5 = QtWidgets.QLabel("-------------------------------------------------------------", self.d1)
+        self.lblLine5.setFont(QFont("Ariel", 20))
+        self.lblLine5.move(xPos, start + dif * 5)
+
+        self.lblLine6 = QtWidgets.QLabel("-------------------------------------------------------------", self.d1)
+        self.lblLine6.setFont(QFont("Ariel", 20))
+        self.lblLine6.move(xPos, start + dif * 6)
+
+        self.lblLine = QtWidgets.QLabel("Avg Fuel Consumption", self.d1)
+        self.lblLine.setFont(QFont("Ariel", 20))
+        self.lblLine.move(xPos, start2)
+
+        self.lblText1 = QtWidgets.QLabel("Fuel Consumption", self.d1)
+        self.lblText1.setFont(QFont("Ariel", 20))
+        self.lblText1.move(xPos, start2 + dif * 1)
+
+        self.lblText2 = QtWidgets.QLabel("Max Speed", self.d1)
+        self.lblText2.setFont(QFont("Ariel", 20))
+        self.lblText2.move(xPos, start2 + dif * 2)
+
+        self.lblText3 = QtWidgets.QLabel("Current Speed", self.d1)
+        self.lblText3.setFont(QFont("Ariel", 20))
+        self.lblText3.move(xPos, start2 + dif * 3)
+
+        self.lblText4 = QtWidgets.QLabel("Avg Speed", self.d1)
+        self.lblText4.setFont(QFont("Ariel", 20))
+        self.lblText4.move(xPos, start2 + dif * 4)
+
+        self.lblText5 = QtWidgets.QLabel("Max RPM", self.d1)
+        self.lblText5.setFont(QFont("Ariel", 20))
+        self.lblText5.move(xPos, start2 + dif * 5)
+
+        self.lblText6 = QtWidgets.QLabel("Current RPM", self.d1)
+        self.lblText6.setFont(QFont("Ariel", 20))
+        self.lblText6.move(xPos, start2 + dif * 6)
+
+        self.lblText7 = QtWidgets.QLabel("Avg RPM", self.d1)
+        self.lblText7.setFont(QFont("Ariel", 20))
+        self.lblText7.move(xPos, start2 + dif * 7)
+
+        self.lblLine10 = QtWidgets.QLabel("KM/L", self.d1)
+        self.lblLine10.setFont(QFont("Ariel", 20))
+        self.lblLine10.move(xPos2, start2)
+
+        self.lblText11 = QtWidgets.QLabel("KM/L", self.d1)
+        self.lblText11.setFont(QFont("Ariel", 20))
+        self.lblText11.move(xPos2, start2 + dif * 1)
+
+        self.lblText12 = QtWidgets.QLabel("KMH", self.d1)
+        self.lblText12.setFont(QFont("Ariel", 20))
+        self.lblText12.move(xPos2, start2 + dif * 2)
+
+        self.lblText13 = QtWidgets.QLabel("KMH", self.d1)
+        self.lblText13.setFont(QFont("Ariel", 20))
+        self.lblText13.move(xPos2, start2 + dif * 3)
+
+        self.lblText14 = QtWidgets.QLabel("KMH", self.d1)
+        self.lblText14.setFont(QFont("Ariel", 20))
+        self.lblText14.move(xPos2, start2 + dif * 4)
+
+        self.lblText15 = QtWidgets.QLabel("RPM", self.d1)
+        self.lblText15.setFont(QFont("Ariel", 20))
+        self.lblText15.move(xPos2, start2 + dif * 5)
+
+        self.lblText16 = QtWidgets.QLabel("RPM", self.d1)
+        self.lblText16.setFont(QFont("Ariel", 20))
+        self.lblText16.move(xPos2, start2 + dif * 6)
+
+        self.lblText17 = QtWidgets.QLabel("RPM", self.d1)
+        self.lblText17.setFont(QFont("Ariel", 20))
+        self.lblText17.move(xPos2, start2 + dif * 7)
+
+        self.d1.exec_()
 
 
+class GUI:
+    def __init__(self,datamanager):
+        app = QApplication(sys.argv)
+        programWindow = ProgramWindow(datamanager)
 
-
-        d1.exec_()
-
-def main():
-    app = QApplication(sys.argv)
-    programWindow = ProgramWindow()
-
-    programWindow.show()
-    sys.exit(app.exec_())
-
-if __name__ == "__main__":
-    main()
+        programWindow.show()
+        sys.exit(app.exec_())
