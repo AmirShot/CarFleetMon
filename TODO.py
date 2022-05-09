35c72a3d2b969c1338e4ef85a34bea73deae3523
Amir <amir2004shot@gmail.com>
2022-05-09 12:34:32 +0300

    step 16 fixed the bug of opening only one live screen at once

---
+++ TODO.py
@@ -0,0 +1,20 @@
+#TODO: update a list of exist live connections
+"""
+adding a list,
+adding a function of OnClose
+"""
+
+#TODO: add a screen and a btn of adding drivers, watch drivers
+
+#TODO: add a function of adding a new vehicle to the fleet
+
+#TODO: fix timing problem
+
+
+
+
+
+#TODO: fix update of main table
+"""
+refresh
+"""
